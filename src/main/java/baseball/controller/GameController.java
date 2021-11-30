670c73baac7afa7597e498dd7cb2400f1412ff45
frogy <baejungeun11@gmail.com>
2021-11-30 15:53:54 +0900

    feat : 1.각기 다른 3자리수 게임 번호 생성

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,8 @@
+package baseball.controller;
+
+public class GameController {
+	//컴퓨터가 게임을 위한 3자리수를 설정하도록 한다.
+	public void pickGameNumber(){
+
+	}
+}
---
+++ src/main/java/baseball/utils/Constant.java
@@ -0,0 +1,11 @@
+package baseball.utils;
+
+public class Constant {
+	//game Rule
+	public static final int GAME_DIGITS = 3;
+	public static final int GAME_MIN_NUMBER = 1;
+	public static final int GAME_MAX_NUMBER = 9;
+
+	//requestMessage
+	public static final String REQUEST_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+}
